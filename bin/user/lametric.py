fcf87f6546b32b250b7fe96388ad18cdd45764ff
Joe Williams <joe@joewilliams.ca>
2022-01-26 09:24:28 -0800

    Update lametric.py
    Python 3 / Weewx 4 support and cleanup

--- bin/user/lametric.py
+++ bin/user/lametric.py
@@ -1,46 +1,79 @@
 #Weewx-lametric connector
-#using an existing extension from Matthew Wall (OWN) to build this one
 
 """
 Send notifications to your LaMetric Time from weewx
 """
 
-from __future__ import absolute_import
-import six.moves.queue
-import base64
-import syslog
-import six.moves.urllib.request, six.moves.urllib.parse, six.moves.urllib.error
-import six.moves.urllib.request, six.moves.urllib.error, six.moves.urllib.parse
+try:
+    # Python 3
+    import queue
+except ImportError:
+    # Python 2
+    import Queue as queue
+try:
+    # Python 3
+    from urllib.request import Request
+except ImportError:
+    # Python 2
+    from urllib2 import Request
 
 
+from base64 import b64encode
+
+import urllib
+import json
 import weewx
 import weewx.restx
 import weewx.units
+import requests
 from weeutil.weeutil import to_bool, accumulateLeaves
 
-VERSION = "0.2"
+    
+    
 
-if weewx.__version__ < "3":
-    raise weewx.UnsupportedFeature("weewx 3 is required, found %s" %
+VERSION = "1.0"
+
+if weewx.__version__ < "4":
+    raise weewx.UnsupportedFeature("weewx 4 is required, found %s" %
                                    weewx.__version__)
 
-def logmsg(level, msg):
-    syslog.syslog(level, 'restx: LaM: %s' % msg)
 
-def logdbg(msg):
-    logmsg(syslog.LOG_DEBUG, msg)
+try:
+    # Test for new-style weewx logging by trying to import weeutil.logger
+    import weeutil.logger
+    import logging
+    log = logging.getLogger(__name__)
 
-def loginf(msg):
-    logmsg(syslog.LOG_INFO, msg)
+    def logdbg(msg):
+        log.debug(msg)
 
-def logerr(msg):
-    logmsg(syslog.LOG_ERR, msg)
+    def loginf(msg):
+        log.info(msg)
 
-class LaMetric(weewx.restx.StdRESTbase):
+    def logerr(msg):
+        log.error(msg)
+
+except ImportError:
+    # Old-style weewx logging
+    import syslog
+    
+    def logdbg(msg):
+       logmsg(syslog.LOG_DEBUG, msg)
+    
+    def logmsg(level, msg):
+       syslog.syslog(level, 'restx: LaM: %s' % msg)
+
+    def loginf(msg):
+       logmsg(syslog.LOG_INFO, msg)
+
+    def logerr(msg):
+       logmsg(syslog.LOG_ERR, msg)
+
+
+
+class LaMetric(weewx.restx.StdRESTful):
     def __init__(self, engine, config_dict):
-        """This service recognizes standard restful options plus the following:
-
-           """
+       
         super(LaMetric, self).__init__(engine, config_dict)
         loginf('service version is %s' % VERSION)
         try:
@@ -56,7 +89,7 @@ class LaMetric(weewx.restx.StdRESTbase):
         site_dict['manager_dict'] = weewx.manager.get_manager_dict(
             config_dict['DataBindings'], config_dict['Databases'], 'wx_binding')
 
-        self.archive_queue = six.moves.queue.Queue()
+        self.archive_queue = queue.Queue()
         self.archive_thread = LaMetricThread(self.archive_queue, **site_dict)
         self.archive_thread.start()
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
@@ -66,8 +99,6 @@ class LaMetric(weewx.restx.StdRESTbase):
         self.archive_queue.put(event.record)
 
 class LaMetricThread(weewx.restx.RESTThread):
-    """
-    """
 
     _SERVER_URL = 'http://%s:8080/api/v2/device/notifications'
     _DATA_MAP = {
@@ -103,7 +134,7 @@ class LaMetricThread(weewx.restx.RESTThread):
         self.icon = icon
         self.sound = sound
         self.server_url = server_url % server_ip
-        #logdbg('server url: %s' % self.server_url)
+        logdbg('server url: %s' % self.server_url)
         self.skip_upload = to_bool(skip_upload)
 
     def process_record(self, record, dbm):
@@ -112,16 +143,15 @@ class LaMetricThread(weewx.restx.RESTThread):
         if self.skip_upload:
             loginf("skipping upload")
             return
-        #logdbg('PR ---- using device_key: %s' % self.device_key)
-        #logdbg('PR ---- using server_url: %s' % self.server_url)
-        req = six.moves.urllib.request.Request(self.server_url, data)
-        req.get_method = lambda: 'POST'
-        req.add_header("User-Agent", "weewx/%s" % weewx.__version__)
-        b64s = base64.encodestring('%s:%s' % ('dev',self.device_key)).replace('\n', '')
-        req.add_header("Authorization", "Basic %s" % b64s)
-        req.add_header("Content-Type", "application/json")
-        self.post_with_retries(req)
+        logdbg('PR ---- using device_key: %s' % self.device_key)
+        logdbg('PR ---- using server_url: %s' % self.server_url)
+       
+        string_key = self.device_key.encode('ascii')
+        userAndPass = b64encode(b"dev:%s" % string_key).decode("ascii")
+        headers = { 'Content-type': 'application/json','Authorization' : 'Basic %s' %  userAndPass }
+        response = requests.post(self.server_url, data=data, headers=headers)
 
+            
     def get_data(self, in_record):
         # put everything into the right units
         record = weewx.units.to_METRIC(in_record)
@@ -131,19 +161,18 @@ class LaMetricThread(weewx.restx.RESTThread):
         for key in self._DATA_MAP:
             rkey = self._DATA_MAP[key][0]
             if rkey in record and record[rkey] is not None:
-              #  logdbg('rkey: %s' % rkey)
-	      #	logdbg('key: %s' % key)
+                logdbg('rkey: %s' % rkey)
+                logdbg('key: %s' % key)
                 v = record[rkey] * self._DATA_MAP[key][2] + self._DATA_MAP[key][3]
- 	      #	logdbg('v: %s' % v) 
+                logdbg('v: %s' % v) 
                 values[key] = self._DATA_MAP[key][1] % v
-	      #	logdbg('values[key]: %s' % values[key])
+                logdbg('values[key]: %s' % values[key])
 
-       # logdbg('data: %s' % values) 
-       # logdbg('Barometer: %s' % values['pressure'])    #20768
-       # logdbg('Temp: %s' % values['temp'])		#2497
-       # logdbg('Wind: %s' % values['wind speed'])	#1153
-       # logdbg('Rain: %s' % values['rain today'])	#72
-       # logdbg('Humidity: %s' % values['humidity'])	#2423
-
-        #return values
-        return " { \"model\": { \"frames\": [{\"icon\": \"%s\", \"text\": \"WeeWx Data\"}, { \"icon\":\"2497\", \"text\":\"%s\"}, { \"icon\":\"20768\", \"text\":\"%s\"}, { \"icon\":\"1153\", \"text\":\"%s\"}, { \"icon\":\"72\", \"text\":\"%s\"}, { \"icon\":\"2423\", \"text\":\"%s\"} ],\"sound\": {\"category\":\"notifications\",\"id\":\"%s\"} } }" % (self.icon, values['temp'], values['pressure'], values['wind speed'], values['rain today'], values['humidity'],  self.sound)
+        logdbg('data: %s' % values) 
+        logdbg('Barometer: %s' % values['pressure'])    #20768
+        logdbg('Temp: %s' % values['temp'])		#2497
+        logdbg('Wind: %s' % values['wind speed'])	#1153
+        logdbg('Rain: %s' % values['rain today'])	#72
+        logdbg('Humidity: %s' % values['humidity'])	#2423
+        return_data = { "model": { "frames": [{"icon": self.icon, "text": "WeeWx Data"}, { "icon":"2497", "text":values['temp']}, { "icon":"20768", "text":values['pressure']}, { "icon":"1153", "text":values['wind speed']}, { "icon":"72", "text":values['rain today']}, { "icon":"2423", "text":values['humidity']} ],"sound": {"category":"notifications","id":self.sound} } } 
+        return json.dumps(return_data)
